ec261a907f5e954853a5030b6e030926982bca46
Brad Weslake <brad.weslake@gmail.com>
2022-11-23 17:20:01 +0800

    Try to fix encoding problems.

--- themes/parallelist/app.rb
+++ themes/parallelist/app.rb
@@ -10,6 +10,8 @@ module Nesta
     #
     # use Rack::Static, :urls => ["/parallelist"], :root => "themes/parallelist/public"
 
+    Encoding.default_external = "UTF-8"
+
     helpers do
       # Use Kramdown for Markdown processing.
       Tilt.prefer Tilt::KramdownTemplate
